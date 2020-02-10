33064934dfb547ac92b967ac120de6a0fa7d9b1e
mxxt1 <matt@dustcloudlabs.com>
2020-02-10 18:45:00 -0500

    12_file_io.py complete

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,27 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+fooFile = open('foo.txt', 'r')
+
+for line in fooFile:
+    print(line, end='')
+
+
+fooFile.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+barFile = open('bar.txt','w+')
+
+for i in range(3):
+    barFile.write("This is an arbitrary line of content number %i\n" %(i))
+
+
+barFile.close()
+
+
