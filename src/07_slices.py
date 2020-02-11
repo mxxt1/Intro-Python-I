e9cb20cf95807c6751a84dd91aab884c94817146
mxxt1 <matt@dustcloudlabs.com>
2020-02-11 14:59:24 -0500

    changed 07_slices to use negative index in slice where appropriate

--- src/07_slices.py
+++ src/07_slices.py
@@ -18,7 +18,8 @@ print(a[1:2])
 print(a[4:5])
 
 # Output the last three elements in the array: [7, 9, 6]
-print(a[3:6])
+# print(a[3:6])
+print(a[-3:])
 
 # Output the two middle elements in the array: [1, 7]
 print(a[2:4])
@@ -27,11 +28,13 @@ print(a[2:4])
 print(a[1:6])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print(a[0:5])
+# print(a[0:5])
+print(a[:-1])
+
 
 # For string s...
 
 s = "Hello, world!"
 
 # Output just the 8th-12th characters: "world"
-print(s[8:12])
+print(s[7:12])
