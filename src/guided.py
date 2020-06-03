9dbfb6c56748b19f77a65fafc479f15fb8d30b9a
Matt Gill <matt@dustcloudlabs.com>
2020-06-02 20:53:40 -0400

    updated guided

--- src/guided.py
+++ src/guided.py
@@ -40,3 +40,31 @@ for (index, element) in enumerate(p):
 
 
 
+#list comprehensions
+#another way to create a list
+
+numbers = [1,2,4,5,6,7,5,4]
+
+#make a new list of squares from the numbers list
+squares = []
+
+for num in numbers:
+    squares.append(num*num)
+print(squares)
+
+# lets use List Comprehensions
+#for each element from numbers multiply the number by itself and add to cooler_squares
+# [*what you want returned* for variable in some_list]
+
+cooler_squares = [num*num for num in numbers]
+print(cooler_squares) 
+
+# let's create a list of just even numbers
+evens = [num*num for num in numbers if num % 2 == 0]
+print(evens)
+
+
+names = ["Sarah", "jorge", "sam", "frank", "bob", "sandy", "shawn"]
+
+s_names = [name.capitalize() for name in names if name[0].lower() == "s" ]
+print(s_names)
